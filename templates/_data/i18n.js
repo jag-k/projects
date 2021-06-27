fd95a66295e2801b6f66307d2b3cc1950cfdf2b4
Jag_k <jag.konon@gmail.com>
2021-06-27 07:29:52 +0300

    Adding emojies :+1:

--- templates/_data/i18n.js
+++ templates/_data/i18n.js
@@ -1,37 +1,37 @@
 module.exports = {
     ru: {
-        workDay: "Время работы над проектом",
+        workDay: "⌛️ Время работы над проектом",
         stillWorking: "По настоящее время",
-        role: "Моя роль в проекте", 
-        company: "Компания / Мероприятие",
-        summary: "Итог",
+        role: "🙋 Моя роль в проекте", 
+        company: "🎒 Компания / Мероприятие",
+        summary: "📝 Итог",
         links: {
-            base: "Ссылки",
-            url: "Сам проект",
+            base: "🔗 Ссылки",
+            url: "Ссылка на проект",
             github: "Репозиторий на GitHub",
             gitlab: "Репозиторий на GitLab",
         },
         stack: {
-            base: "Стек проекта",
+            base: "💻 Стек проекта",
             languages: "Языки программирования",
             frameworks: "Фреймворки и библиотеки",
             technologies: "Технологии, рабочие процессы и прочее"
         }
     },
     en: {
-        workDay: "Working on project",
+        workDay: "⌛️ Working on project",
         stillWorking: "Until now",
-        role: "My role in the project", 
-        company: "Company / Event",
-        summary: "Summary",
+        role: "🙋 My role in the project", 
+        company: "🎒 Company / Event",
+        summary: "📝 Summary",
         links: {
-            base: "Links",
+            base: "🔗 Links",
             url: "Link to project",
             github: "Repository on GitHub",
             gitlab: "Repository on GitLab",
         },
         stack: {
-            base: "Stack of project",
+            base: "💻 Stack of project",
             languages: "Programming languages",
             frameworks: "Frameworks and libraries",
             technologies: "Technologies, work processes and other"
